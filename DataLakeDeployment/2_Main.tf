bf776b683260e888827d0371afeabbd381b8c5c9
arnaud_pesenti <arnaud_pesenti@yahoo.fr>
2024-11-05 11:58:45 +0000

    TERRAFORM PROJECT UPDATE: Container deployment in the data lake and the basic blob storage complete

--- DataLakeDeployment/2_Main.tf
+++ DataLakeDeployment/2_Main.tf
@@ -38,14 +38,14 @@ resource "azurerm_resource_group" "ResourcesGroup" {
 # }
 
 # BASIC BLOB STORAGE SETTING UP
-# >>> created as a 'standard' storage account
+# >>> created as a 'standard' storage account.
 resource "azurerm_storage_account" "BlobStorage" {
   name                      = var.BasicBlobStorageName
   resource_group_name       = azurerm_resource_group.ResourcesGroup.name
   location                  = azurerm_resource_group.ResourcesGroup.location
   account_tier              = "Standard"
   account_replication_type  = "LRS"
-  access_tier               = "Hot" # 'hot' means frequent accessed data
+  access_tier               = "Hot" # 'hot' means frequent accessed data.
 
   # Keeps standard storage account
   account_kind              = "BlobStorage"
@@ -54,30 +54,31 @@ resource "azurerm_storage_account" "BlobStorage" {
 
 
 # DATA LAKE SETTING UP
-# >>> created as a 'standard' storage account with Hierarchical Namespace Enabled
+# >>> created as a 'standard' storage account with Hierarchical Namespace Enabled.
 resource "azurerm_storage_account" "DataLake" {
   name                      = var.DataLakeName
   resource_group_name       = azurerm_resource_group.ResourcesGroup.name
   location                  = azurerm_resource_group.ResourcesGroup.location
   account_tier              = "Standard"
   account_replication_type  = "LRS"
-  access_tier               = "Hot" # 'hot' means frequent accessed data
+  access_tier               = "Hot" # 'hot' means frequent accessed data.
 
-  # Changes standard storage account into a data lake type storage
+  # Changes standard storage account into a data lake type storage.
   account_kind              = "StorageV2"
-  is_hns_enabled            = true # Enabling Hierarchical Namespace as it is necessary for Data Lake Gen2
+  is_hns_enabled            = true # Enabling Hierarchical Namespace as it is necessary for Data Lake Gen2.
 }
-
-# # Step 5: Create a File System (Container) within the Storage Account
-# resource "azurerm_storage_data_lake_gen2_filesystem" "filesystem" {
-#   name               = var.file_system_name
-#   storage_account_id = azurerm_storage_account.storage.id
-# }
-
-# # Step 6: Output the Storage Account details for confirmation
-# output "storage_account_name" {
-#   value = azurerm_storage_account.storage.name
-# }
+# >>> Adds a container called "datastorage" within the data lake.
+resource "azurerm_storage_data_lake_gen2_filesystem" "datastorage" {
+  name                      = "datastorage"
+  storage_account_id        = azurerm_storage_account.DataLake.id
+}
+# >>> Adds a directory called "input_data_portal" whithin the "datastorage" container.
+resource "azurerm_storage_data_lake_gen2_path" "datastorageDir1" {
+  path               = "input_data_portal"
+  filesystem_name    = azurerm_storage_data_lake_gen2_filesystem.datastorage.name
+  storage_account_id = azurerm_storage_account.DataLake.id
+  resource           = "directory"    # Resource type. So 'directory' here!
+}
 
 # output "storage_account_id" {
 #   value = azurerm_storage_account.storage.id
